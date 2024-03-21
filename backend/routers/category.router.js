561ff8038461a00f357dfeaa95fc6f424b32f052
Your Name <ciftci75can@gmail.com>
2024-03-21 22:55:03 +0300

    .

---
+++ backend/routers/category.router.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const Category = require("../models/category");
+const {v4:uuidv4} = require("uuid");
+
+router.post("/add", async (req, res)=> {
+    try {
+        const {name} = req.body;
+        
+        const checkName = await Category.findOne({name: name});
+        if (checkName != null) {
+            res.status(403).json({message: "Bu kategori adı daha önce kullanılmış"});
+        } else {
+            const category = new Category({
+                _id: uuidv4(),
+                name: name
+            });
+    
+            await category.save();
+            res.json({message: "Kategori kaydı başarıyla tamamlandı"}); 
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+router.post("/removeById", async (req, res)=> {
+    try {
+        const {_id} = req.body;
+        await Category.findByIdAndDelete(_id);
+        res.json({message: "Kategori kaydı başarıyla silindi"});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+router.post("/update", async (req, res)=> {
+    try {
+        const {_id,name} = req.body;
+        const category = await Category.findOne({_id: _id});
+        
+        if (category.name != name) {
+            const checkName = await Category.findOne({name: name});
+            if (checkName != null) {
+                res.status(403).json({message: "Bu kategori adı daha önce kullanılmış"});
+            } else {
+                category.name = name;
+        
+                await Category.findByIdAndUpdate(_id, category);
+                res.json({message: "Kategori kaydı başarıyla güncellendi"});
+            }
+        }else{
+            return res.status(403).json({ message: "Yeni kategori adı mevcut kategori adıyla aynı olamaz" });
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+router.get("/", async (req, res)=> {
+    try {
+        const categories = await Category.find().sort({name: 1});
+        res.json(categories);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+module.exports = router;
--- frontend/src/app/components/admin-category/admin-category.component.ts
+++ frontend/src/app/components/admin-category/admin-category.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryModel } from './models/category.model';
+import { ToastrService } from 'ngx-toastr';
+import { CategoryService } from './services/category.service';
+import { SharedModule } from '../../common/shared/shared.module';
+import { NgForm } from '@angular/forms';
+import { SwalService } from '../../common/services/swal.service';
 
 @Component({
   selector: 'app-admin-category',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './admin-category.component.html',
   styleUrl: './admin-category.component.css'
 })
-export class AdminCategoryComponent {
+export class AdminCategoryComponent implements OnInit{
+  categories: CategoryModel[] = [];
+  updateCategory: CategoryModel = new CategoryModel();
 
+  constructor(
+    private _toastr: ToastrService,
+    private _category: CategoryService,
+    private _swal: SwalService
+  ){}
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(){
+    this._category.getAll(res=> this.categories = res);
+  }
+
+  add(form:NgForm){
+    if (form.valid) {
+      this._category.add(form.controls["name"].value,res=>{
+        this._toastr.success(res.message);
+        let element = document.getElementById("addModelCloseBtn");
+        element?.click();
+        form.reset();
+        this.getAll();
+      });
+    }
+  }
+
+  get(model: CategoryModel){
+    this.updateCategory = {...model};
+  }
+  
+  update(form:NgForm){
+    if (form.valid) {
+      this._category.update(this.updateCategory, res=>{
+        this._toastr.warning(res.message);
+        this.getAll();
+        let element = document.getElementById("updateModalCloseBtn");
+        element?.click();
+      })
+    }
+  }
+
+  removeById(model: CategoryModel){
+    this._swal.callSwal(`${model.name} kategorisini silmek istiyormusunuz`, "" ,"Sil",()=>{
+      this._category.removeById(model._id,res=>{
+        this._toastr.info(res.message);
+        this.getAll();
+      })
+    });
+  }
 }
